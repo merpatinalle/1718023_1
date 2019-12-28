b329605e83605c39606319c12c736ca9d0cd6033
merta <merpatinalle02@gmail.com>
2019-12-27 18:08:29 -0800

    Merpati_1718023

---
+++ app/src/main/java/com/example/a1718023/App/AppSeting.java
@@ -0,0 +1,61 @@
+package com.example.a1718023.App;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import com.example.a1718023.R;
+
+import java.util.Objects;
+
+public class AppSeting extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_seting);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new MyPreferenceFargmnet())
+                .commit();
+    }
+
+    public static class MyPreferenceFargmnet extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+    String setting_locale;
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.app_settings);
+            setPreferencesFromResource(R.xml.app_settings, rootKey);
+            setting_locale = this.getResources().getString(R.string.key_setting_locale);
+            Objects.requireNonNull(findPreference(setting_locale)).setOnPreferenceClickListener(this);
+
+        }
+
+         @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+           return false;
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            String key = preference.getKey();
+
+            if (key.equals(setting_locale)){
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
--- app/src/main/java/com/example/a1718023/MainActivity.java
+++ app/src/main/java/com/example/a1718023/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.a1718023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.a1718023.App.AppSeting;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void showsetting(){
+        setContentView(R.layout.activity_app_seting);
+    }
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+    setMode(item.getItemId());
+    return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode){
+        switch (selectedMode){
+            case R.id.bahasa:
+                Intent setting = new Intent(MainActivity.this, AppSeting.class);
+                startActivity(setting);
+                return;
+        }
     }
 }
